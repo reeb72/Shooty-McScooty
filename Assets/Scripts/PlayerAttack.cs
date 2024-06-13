02f3bd0e68637953033bd1c027c47b81f7101f21
joshywashy2 <124170057+joshywashy2@users.noreply.github.com>
2024-06-12 23:28:18 -0400

    Update PlayerAttack.cs

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+    public float fireballDamage = 8.0f; // Damage inflicted by fireball
+    public float laserDamage = 12.0f;  
     public float damage = 5.0f;
     void OnTriggerEnter2D(Collider2D collision)
     {
@@ -15,6 +17,20 @@ public class PlayerAttack : MonoBehaviour
                 health.TakeDamage(damage);// Deal 5 damage
             }
         }
+        
+        else if (collision.gameObject.CompareTag("Boss")) // Check if collided with the boss
+        {
+            // Determine the type of attack based on the collided object's tag
+            if (collision.gameObject.CompareTag("Fireball"))
+            {
+                TakeDamage(fireballDamage); // Inflict fireball damage
+            }
+            else if (collision.gameObject.CompareTag("Laser"))
+            {
+                TakeDamage(laserDamage); // Inflict laser damage
+            }
+        }
+
         if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Enemy") 
         || collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Bullet")
         || collision.gameObject.CompareTag("Cactus"))
